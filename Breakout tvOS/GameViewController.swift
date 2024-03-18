ba93501b52c109abc4dd5fbb1169e99ed9067d44
William Dunkel <wndunkel@gmail.com>
2024-03-18 09:35:58 -0500

    Initial Commit

---
+++ Breakout tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Breakout tvOS
+//
+//  Created by William Dunkel on 3/18/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
